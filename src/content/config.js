77ce80c0268f86a9db45fe50a810940f9e3f8c0c
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-04-11 20:19:13 +0200

    config.js bugfix

--- src/content/config.js
+++ src/content/config.js
@@ -2,14 +2,16 @@ import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
   schema: z.object({
+    published: z.boolean(),
     title: z.string(),
-    // description: z.string(),
+    description: z.string(),
+    author: z.string(),
     date: z.coerce.date(),
-    // updatedDate: z.coerce.date().optional(),
-    // featuredImage: z.string().optional(),
-    // tags: z.string().array().optional(),
-    // author: z.string(),
-    // isFeatured: z.boolean(),
+    tags: z.string().array().optional(),
+    date: z.coerce.date().optional(),
+    thumbnail: z.string().optional(),
+    thumbnailMobile: z.string().optional(),
+    content: z.string(),
   }),
 });
 
--- src/content/posts/2nd-blog-post.md
+++ src/content/posts/2nd-blog-post.md
@@ -3,10 +3,10 @@ published: true
 title: 2nd blog post
 description: This is just a test
 author: Kisanna
-tags:
-  - testblog
+tags: ['tag1', 'tag2']
 date: 2024-04-04T17:37:00.000Z
 thumbnail: src/images/uploads/desktop_img.jpg
 thumbnailMobil: src/images/uploads/mobil_img.jpg
 ---
+
 This is just a 2nd test
