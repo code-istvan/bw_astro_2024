f617744213da13be268d04ff6d3467d100045a1f
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-04-12 12:20:31 +0200

    config.yml changes

--- src/content/config.js
+++ src/content/config.js
@@ -1,20 +1,20 @@
-import { defineCollection, z } from 'astro:content';
+// import { defineCollection, z } from 'astro:content';
 
-const posts = defineCollection({
-  schema: z.object({
-    published: z.boolean(),
-    title: z.string(),
-    description: z.string(),
-    author: z.string(),
-    date: z.coerce.date(),
-    tags: z.string().array().optional(),
-    date: z.coerce.date().optional(),
-    thumbnail: z.string().optional(),
-    thumbnailMobile: z.string().optional(),
-    content: z.string(),
-  }),
-});
+// const posts = defineCollection({
+//   schema: z.object({
+//     published: z.boolean(),
+//     title: z.string(),
+//     description: z.string(),
+//     author: z.string(),
+//     date: z.coerce.date(),
+//     tags: z.string().array().optional(),
+//     date: z.coerce.date().optional(),
+//     thumbnail: z.string().optional(),
+//     thumbnailMobile: z.string().optional(),
+//     content: z.string(),
+//   }),
+// });
 
-export const collections = {
-  posts: posts,
-};
+// export const collections = {
+//   posts: posts,
+// };
