edfa0eb5d677d08bb0fce3ec9cb3953c8c496afe
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-04-13 17:26:05 +0200

    Reading time component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './remark-reading-time.mjs';
+import react from '@astrojs/react';
 
 export default defineConfig({
-    i18n: {
-        defaultLocale: "hu",
-        locales: ["hu", "en"],
-        routing: {
-            prefixDefaultLocale: false,
-        },
+  i18n: {
+    defaultLocale: 'hu',
+    locales: ['hu', 'en'],
+    routing: {
+      prefixDefaultLocale: false,
     },
-
-    integrations: [react()],
+  },
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
+  integrations: [react()],
 });
