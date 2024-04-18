5d572dc0ad908fe2789b77928aca2a8377f5d971
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-04-18 18:49:04 +0200

    switch off rss

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -1,23 +1,45 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+// import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export async function GET(context) {
-  const blog = await getCollection('blog');
-  return rss({
-    title: 'Buzz’s Blog',
-    description: 'A humble Astronaut’s guide to the stars',
-    site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      //   pubDate: post.data.pubDate,
-      description: post.data.description,
-      //   customData: post.data.customData,
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
-      link: `/blog/${post.slug}/`,
-    })),
-  });
-}
+// const allPosts = await pagesGlobToRssItems(import.meta.glob('./*.md')),
+
+// const postImportResult = import.meta.glob('./blog/**/*.md', { eager: true });
+// const posts = allPosts(Object.values(postImportResult));
+
+// export const get = () =>
+//   rss({
+//     stylesheet: '/rss/styles.xsl',
+//     title: 'My Astro Blog',
+//     description: 'A humble Astronaut’s guide to the stars',
+//     site: import.meta.env.SITE,
+//     items: posts.map((post) => ({
+//       link: post.url,
+//       title: post.frontmatter.title,
+//       pubDate: post.frontmatter.date,
+//       description: post.frontmatter.description,
+//       customData: `
+//         <author>${post.frontmatter.author}</author>
+//       `,
+//     })),
+//   });
+
+// import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+// export async function GET(context) {
+//   const blog = import.meta.glob('./*.md');
+//   const posts = Object.values(blog);
+//   console.log(await pagesGlobToRssItems(import.meta.glob('/blog/*.md')));
+
+//   console.log(posts);
+//   return rss({
+//     title: 'Buzz’s Blog',
+//     description: 'A humble Astronaut’s guide to the stars',
+//     site: context.site,
+//     // items: await pagesGlobToRssItems(import.meta.glob('./*.md')),
+//     items: posts.map((post) => ({
+//       pubDate: post.date,
+//     })),
+//   });
+// }
 
 // import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
@@ -26,7 +48,7 @@ export async function GET(context) {
 //     title: 'Bandha Works Jógaiskola | Blog',
 //     description: 'Astanga jóga blog',
 //     site: context.site,
-//     items: await pagesGlobToRssItems(import.meta.glob('/**/*.md')),
+// items: await pagesGlobToRssItems(import.meta.glob('/**/*.md')),
 //     customData: `<language>hu-hu</language>`,
 //   });
 // }
