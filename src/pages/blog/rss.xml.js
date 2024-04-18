103272cd6386f49c6ce1038b4c599d8d4eae600a
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-04-18 17:52:59 +0200

    rss refresh

--- src/pages/blog/rss.xml.js
+++ src/pages/blog/rss.xml.js
@@ -1,11 +1,32 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const blog = await getCollection('blog');
   return rss({
-    title: 'Bandha Works Jógaiskola | Blog',
-    description: 'Astanga jóga blog',
+    title: 'Buzz’s Blog',
+    description: 'A humble Astronaut’s guide to the stars',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('/**/*.md')),
-    customData: `<language>hu-hu</language>`,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      //   pubDate: post.data.pubDate,
+      description: post.data.description,
+      //   customData: post.data.customData,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/blog/${post.slug}/`,
+    })),
   });
 }
+
+// import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+// export async function GET(context) {
+//   return rss({
+//     title: 'Bandha Works Jógaiskola | Blog',
+//     description: 'Astanga jóga blog',
+//     site: context.site,
+//     items: await pagesGlobToRssItems(import.meta.glob('/**/*.md')),
+//     customData: `<language>hu-hu</language>`,
+//   });
+// }
