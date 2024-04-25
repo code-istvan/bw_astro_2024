8806ab9dd8df15f1c5da1ef4a14eab1ca3df5003
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-04-25 14:28:31 +0200

    huge blog update

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content';
+
+const postsCollection = defineCollection({
+  schema: z.object({
+    published: z.boolean(),
+    title: z.string(),
+    description: z.string(),
+    author: z.string(),
+    tags: z.string().array().optional(),
+    date: z.coerce.date(),
+    // date: z.coerce.date().optional(),
+    image: z.string().optional(),
+    imageMobil: z.string().optional(),
+    // content: z.string(),
+  }),
+});
+
+export const collections = {
+  blog: postsCollection,
+};
