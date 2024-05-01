1783858d369f43ebc311583981531359e5789e2b
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-05-01 13:30:15 +0200

    Creating dinamic author page

--- src/components/js/utils.js
+++ src/components/js/utils.js
@@ -2,6 +2,8 @@ export function slugify(text) {
   return text
     .toString()
     .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
     .replace(/\s+/g, '-')
     .replace(/[^\w-]+/g, '')
     .replace(/--+/g, '-')
