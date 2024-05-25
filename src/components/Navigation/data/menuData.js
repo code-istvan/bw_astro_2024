a255ba99aa5ee24fa1b3e460ac4932a079b3a09b
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-05-25 12:27:50 +0200

    add hero images to EN pages

--- src/components/Navigation/data/menuData.js
+++ src/components/Navigation/data/menuData.js
@@ -1,42 +1,42 @@
 export const getMenuItemsJogaorak = (translatePath, t) => [
-    { link: translatePath(`/orarend/`), label: t("orarend") },
-    { link: translatePath(`/arak/`), label: t("arak") },
-    {
-        link: translatePath(`/astanga-mysore-program/`),
-        label: t("mysoreprogram"),
-    },
-    { link: translatePath(`/tanfolyam/`), label: t("tanfolyamok") },
-    { link: translatePath(`/csapatunk/`), label: t("csapatunk") },
-    {
-        link: translatePath(`/vinyasza-jogairanyzatok/`),
-        label: t("jógairányzatok"),
-    },
-    { link: translatePath(`/letoltesek/`), label: t("letoltesek") },
-    // { link: "/galeria", label: "GALÉRIA", extraClass: "" },
+  { link: translatePath(`/orarend/`), label: t('orarend') },
+  { link: translatePath(`/arak/`), label: t('arak') },
+  {
+    link: translatePath(`/astanga-mysore-program/`),
+    label: t('mysoreprogram'),
+  },
+  { link: translatePath(`/tanfolyam/`), label: t('tanfolyamok') },
+  { link: translatePath(`/csapatunk/`), label: t('csapatunk') },
+  {
+    link: translatePath(`/vinyasza-jogairanyzatok/`),
+    label: t('jogairanyzatok'),
+  },
+  { link: translatePath(`/letoltesek/`), label: t('letoltesek') },
+  // { link: "/galeria", label: "GALÉRIA", extraClass: "" },
 ];
 
 export const getMenuItemsTudnivalok = (translatePath, t) => [
-    { link: translatePath(`/rolunk/`), label: t("rolunk") },
-    { link: translatePath(`/elso-alkalom/`), label: t("elsoalkalom") },
-    { link: translatePath(`/hazirend/`), label: t("hazirend") },
-    { link: translatePath(`/mantra/`), label: t("mantra") },
-    { link: translatePath(`/holdnapok/`), label: t("holdnapok") },
-    {
-        link: translatePath(`/osztondij/`),
-        label: t("osztondij"),
-    },
-    // { link: "/taplalkozas", label: "TÁPLÁLKOZÁS" },
-    {
-        link: translatePath(`/ajanlott-olvasmanyok/`),
-        label: t("olvasmanyok"),
-    },
+  { link: translatePath(`/rolunk/`), label: t('rolunk') },
+  { link: translatePath(`/elso-alkalom/`), label: t('elsoalkalom') },
+  { link: translatePath(`/hazirend/`), label: t('hazirend') },
+  { link: translatePath(`/mantra/`), label: t('mantra') },
+  { link: translatePath(`/holdnapok/`), label: t('holdnapok') },
+  {
+    link: translatePath(`/osztondij/`),
+    label: t('osztondij'),
+  },
+  // { link: "/taplalkozas", label: "TÁPLÁLKOZÁS" },
+  {
+    link: translatePath(`/ajanlott-olvasmanyok/`),
+    label: t('olvasmanyok'),
+  },
 ];
 
 export const getMenuItemsFomenu = (translatePath, t) => [
-    { link: "/blog", label: "BLOG" },
-    {
-        link: "https://shop.bandha.works",
-        label: "SHOP",
-    },
-    { link: translatePath(`/kapcsolat/`), label: t("kapcsolat") },
+  { link: '/blog', label: 'BLOG' },
+  {
+    link: 'https://shop.bandha.works',
+    label: 'SHOP',
+  },
+  { link: translatePath(`/kapcsolat/`), label: t('kapcsolat') },
 ];
