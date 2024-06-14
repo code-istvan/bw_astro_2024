10b04b9ff97b659641b31d1f6558e7fb0bb62c23
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-06-14 12:56:08 +0200

    Button.jsx bugfix

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,38 +1,26 @@
-import "../../sass/components/_button.scss";
+import '../../sass/components/_button.scss';
 
 const STYLES = [
-    "btn--primary--solid",
-    "btn--primary--solid--full",
-    "btn--plain--naked",
-    "btn--secondary--solid",
-    "btn--secondary--solid--full",
-    "btn--secondary--outline",
-    "btn--third--solid",
-    "btn--third--outline",
-    "btn--third--naked-orange",
-    "btn--primary--outline",
-    "btn--primary--outline-full",
-    "btn--small--outline",
+  'btn--primary--solid',
+  'btn--primary--solid--full',
+  'btn--plain--naked',
+  'btn--secondary--solid',
+  'btn--secondary--solid--full',
+  'btn--secondary--outline',
+  'btn--third--solid',
+  'btn--third--outline',
+  'btn--third--naked-orange',
+  'btn--primary--outline',
+  'btn--primary--outline-full',
+  'btn--small--outline',
 ];
 
-export default function Button({
-    children,
-    type,
-    onClick,
-    buttonStyle,
-    class = "",
-}) {
-    const checkButtonStyle = STYLES.includes(buttonStyle)
-        ? buttonStyle
-        : STYLES[0];
+export default function Button({ children, type, onClick, buttonStyle, className = '' }) {
+  const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
-    return (
-        <button
-            class={`btn ${checkButtonStyle} ${class}`}
-            onClick={onClick}
-            type={type}
-        >
-            {children}
-        </button>
-    );
+  return (
+    <button class={`btn ${checkButtonStyle} ${className}`} onClick={onClick} type={type}>
+      {children}
+    </button>
+  );
 }
