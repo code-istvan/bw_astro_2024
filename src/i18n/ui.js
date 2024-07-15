829ce13b855aba5099afe1501d7a51df6ab3f751
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-07-15 22:09:02 +0200

    Bilingualisation of SectionMysoreProgramme

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -25,10 +25,15 @@ export const ui = {
     jogaorak: 'JÓGAÓRÁK',
     tudnivalok: 'TUDNIVALÓK',
     kapcsolat: 'KAPCSOLAT',
-    paramparaText:
+    sectionParamparaText:
       'A parampara a tudás, amelyet a tanár a tanítványára hagyományoz. A parampara szanszkrit szó jelentése a tudás átadásának legértékesebb formáját, a közvetlen és tapasztalati tudás átadását fejezi ki. A jógatanításnak a paramparaból kell származnia ahhoz, hogy hatékony, igaz és teljes legyen. ',
-    paramparaButton: 'Tanítói láncolatunk',
+    sectionParamparaButton: 'Tanítói láncolatunk',
+    sectionMysoreProgramTitle: 'Astanga Mysore-program',
+    sectionMysoreProgramText:
+      'A Mysore-stílusú astanga jóga gyakorlás hatékony és személyre szabott módszer, amely fizikai erőt, rugalmasságot és belső nyugalmat hoz. Az általa kínált folyamatos fejlődés és az oktató-tanítvány kapcsolat lehetőséget ad a mélyebb megértésre és a személyes fejlődésre.',
+    sectionMysoreProgramButton: 'Mysore-programról bővebben',
   },
+
   en: {
     orarend: 'Schedule',
     arak: 'Prices',
@@ -47,7 +52,11 @@ export const ui = {
     jogaorak: 'YOGA CLASSES',
     tudnivalok: 'INFORMATION',
     kapcsolat: 'CONTACT',
-    paramparaText: `Parampara is the knowledge that the teacher passes on to the student. The word parampara in Sanskrit expresses the most valuable form of knowledge transfer, the direct and experiential transfer of knowledge. Yoga teaching must come from the parampara to be effective, true and complete.`,
-    paramparaButton: 'Our lineage',
+    sectionParamparaText: `Parampara is the knowledge that the teacher passes on to the student. The word parampara in Sanskrit expresses the most valuable form of knowledge transfer, the direct and experiential transfer of knowledge. Yoga teaching must come from the parampara to be effective, true and complete.`,
+    sectionParamparaButton: 'Our lineage',
+    sectionMysoreProgramTitle: 'Ashanga Mysore program',
+    sectionMysoreProgramText:
+      'The Mysore-style Astanga yoga practice is an effective and personalized method that brings physical strength, flexibility and inner calm. The continuous development it offers and the teacher-student relationship provide an opportunity for deeper understanding and personal development.',
+    sectionMysoreProgramButton: 'Ashtanga Mysore program',
   },
 };
