b7ac1270693836cf1491b85695319edd9c8d1be5
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-07-22 22:57:14 +0200

    Carousel dinamic data injection

--- src/components/Carousel/Carousel.ts
+++ src/components/Carousel/Carousel.ts
@@ -1,19 +1,3 @@
-// import EmblaCarousel from 'embla-carousel';
-// import type { EmblaOptionsType } from 'embla-carousel';
-// import { addDotBtnsAndClickHandlers } from './EmblaCarouselDotButton.ts';
-
-// const OPTIONS: EmblaOptionsType = { loop: true };
-
-// const emblaNode = <HTMLElement>document.querySelector('.embla');
-// const viewportNode = <HTMLElement>emblaNode.querySelector('.embla__viewport');
-// const dotsNode = <HTMLElement>emblaNode.querySelector('.embla__dots');
-
-// const emblaApi = EmblaCarousel(viewportNode, OPTIONS);
-
-// const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(emblaApi, dotsNode);
-
-// emblaApi.on('destroy', removeDotBtnsAndClickHandlers);
-
 import EmblaCarousel from 'embla-carousel';
 import type { EmblaOptionsType } from 'embla-carousel';
 import { addDotBtnsAndClickHandlers } from './EmblaCarouselDotButton.ts';
