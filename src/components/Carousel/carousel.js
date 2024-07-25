588f92de5495b32e279578c6e6a020e40b180701
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-07-25 23:23:24 +0200

    Carousel external scripts

--- src/components/Carousel/carousel.js
+++ src/components/Carousel/carousel.js
@@ -1,4 +1,6 @@
-export const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
+import EmblaCarousel from 'embla-carousel';
+
+const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
   let dotNodes = [];
 
   const addDotBtnsWithClickHandlers = () => {
@@ -35,3 +37,16 @@ export const addDotBtnsAndClickHandlers = (emblaApi, dotsNode) => {
     dotsNode.innerHTML = '';
   };
 };
+
+export function initCarousel() {
+  const OPTIONS = { loop: true };
+
+  const emblaNode = document.querySelector('.embla');
+  const viewportNode = emblaNode.querySelector('.embla__viewport');
+  const dotsNode = emblaNode.querySelector('.embla__dots');
+
+  const emblaApi = EmblaCarousel(viewportNode, OPTIONS);
+  const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(emblaApi, dotsNode);
+
+  emblaApi.on('destroy', removeDotBtnsAndClickHandlers);
+}
