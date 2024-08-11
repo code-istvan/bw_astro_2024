2400d76865ea54d5038907765d13f747e9294001
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-08-11 16:37:38 +0200

    Section links improvment

--- src/i18n/ui.js
+++ src/i18n/ui.js
@@ -28,12 +28,15 @@ export const ui = {
     sectionParamparaText:
       'A parampara a tudás, amelyet a tanár a tanítványára hagyományoz. A parampara szanszkrit szó jelentése a tudás átadásának legértékesebb formáját, a közvetlen és tapasztalati tudás átadását fejezi ki. A jógatanításnak a paramparaból kell származnia ahhoz, hogy hatékony, igaz és teljes legyen. ',
     sectionParamparaButton: 'Tanítói láncolatunk',
+    sectionParamparaLink: '/parampara',
     sectionMysoreProgramTitle: 'Astanga Mysore-program',
     sectionMysoreProgramText:
       'A Mysore-stílusú astanga jóga gyakorlás hatékony és személyre szabott módszer, amely fizikai erőt, rugalmasságot és belső nyugalmat hoz. Az általa kínált folyamatos fejlődés és az oktató-tanítvány kapcsolat lehetőséget ad a mélyebb megértésre és a személyes fejlődésre.',
     sectionMysoreProgramButton: 'Mysore-programról bővebben',
+    sectionMysoreProgramLink: '/astanga-mysore-program',
     sectionScholarshipTitle: 'Jóga ösztöndíj',
     sectionScholarshipButton: 'Jóga ösztöndíj',
+    sectionScholarshipLink: '/osztondij',
     sectionScholarshipText: `A jóga mindenki életében gyökeres változásokat hozhat, gyakorlását bármely élethelyzetben érdemes
                 elkezdeni. Előfordulhat azonban, hogy anyagi körülményeink nem teszik lehetővé hogy tapasztalt és
                 hiteles jógatanároktól tanulhassunk. A Bandha Works Ösztöndíj éppen az ilyen helyzetekben nyújthat
@@ -61,12 +64,15 @@ export const ui = {
     kapcsolat: 'CONTACT',
     sectionParamparaText: `Parampara is the knowledge that the teacher passes on to the student. The word parampara in Sanskrit expresses the most valuable form of knowledge transfer, the direct and experiential transfer of knowledge. Yoga teaching must come from the parampara to be effective, true and complete.`,
     sectionParamparaButton: 'Our lineage',
+    sectionParamparaLink: '/en/parampara',
     sectionMysoreProgramTitle: 'Ashanga Mysore program',
     sectionMysoreProgramText:
       'The Mysore-style Astanga yoga practice is an effective and personalized method that brings physical strength, flexibility and inner calm. The continuous development it offers and the teacher-student relationship provide an opportunity for deeper understanding and personal development.',
     sectionMysoreProgramButton: 'Ashtanga Mysore program',
+    sectionMysoreProgramLink: '/en/astanga-mysore-program',
     sectionScholarshipTitle: 'Scholarship',
     sectionScholarshipButton: 'Scholarship details',
+    sectionScholarshipLink: '/en/osztondij',
     sectionScholarshipText: `Yoga can bring radical changes in everyone's life, and it is worth starting at any stage of life. However, sometimes our financial circumstances do not allow us to buy a yoga pass, and we may not yet have the routine to start practising at home. The Bandha Works Scholarship can help in exactly such situations, with our yoga school providing the winner with unlimited yoga pass for three months.`,
     sectionSocialProofsTitle: 'Testimonials',
   },
