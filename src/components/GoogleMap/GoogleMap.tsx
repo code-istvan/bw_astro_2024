311d44e21583d6fc58955fb08483e418bed1c1e9
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-09-05 18:17:36 +0200

    feat: add GoogleMap configuration

---
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { MiniLogo } from './MiniLogo.tsx';
+import MapStyle from './MapStyle';
+import '../../sass/components/_googlemap.scss';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+const defaultProps = {
+  center: {
+    lat: 47.51416,
+    lng: 19.03734,
+  },
+  zoom: 15,
+};
+
+const GoogleMap = () => (
+  <div className="googleMap__box container-fluid">
+    <GoogleMapReact
+      bootstrapURLKeys={{ key: import.meta.env.PUBLIC_GOOGLE_MAP_API_KEY }}
+      defaultCenter={defaultProps.center}
+      defaultZoom={defaultProps.zoom}
+      options={{ styles: MapStyle }}
+    >
+      {/* @ts-ignore */}
+      <MiniLogo lat={47.51416} lng={19.03734} text={'Bandha Works'} />
+    </GoogleMapReact>
+  </div>
+);
+
+export default GoogleMap;
