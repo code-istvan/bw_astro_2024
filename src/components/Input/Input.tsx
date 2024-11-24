fdfb25e0e55a7901c82b264299a3a75f0bee933e
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-11-24 20:45:21 +0100

    feat: create Input and Textarea component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,16 @@
+import './_input.scss';
+
 interface InputProps {
   type: string;
   placeholder: string;
   id: string;
+  className?: string;
 }
 
-export const Input = ({ type, placeholder, id }: InputProps) => {
+export const Input = ({ type, placeholder, id, className }: InputProps) => {
   return (
     <label htmlFor={id}>
-      <input type={type} id={id} placeholder={placeholder} />
+      <input type={type} id={id} placeholder={placeholder} className={`input ${className || ''}`.trim()} />
     </label>
   );
 };
