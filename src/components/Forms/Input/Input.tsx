fa9fb4a0494034577102d816715603357a0ecb5b
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-11-27 20:35:14 +0100

    fix: netlify form sending

--- src/components/Forms/Input/Input.tsx
+++ src/components/Forms/Input/Input.tsx
@@ -15,7 +15,7 @@ export const Input = ({ type, name, placeholder, id, className, required, patter
         <input
           type={type}
           name={name}
-          id={id}
+          id={id || name}
           placeholder={placeholder}
           className={`input ${className || ''}`.trim()}
           required={!!required}
