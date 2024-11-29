2cfb005fef7ecfd4804aa3b5588f4051b50c0ffe
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-11-29 15:56:18 +0100

    feat: upload Hirlevel page content and add a Newsletter form to is

---
+++ src/components/Forms/NewsletterForm.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { TextArea } from './TextArea/TextArea';
+import { Checkbox } from './Checkbox/CheckBox';
+import { Input } from './Input/Input';
+
+export const NewsletterForm = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <form
+      name="newsletter bandhaworks 2025"
+      action="/message-sent"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="form-name" value="newsletter bandhaworks 2025" />
+      <div hidden>
+        <input name="bot-field" />
+      </div>
+      <div className="row gap-1 mt-20px mb-20px">
+        <div className="col-12-xs col-6-md">
+          <Input
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Név"
+            pattern="^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ\s]+$"
+            required
+          />
+        </div>
+        <div className="col-12-xs col-6-md">
+          <Input
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Email"
+            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+            required
+          />
+        </div>
+      </div>
+      <div className="row mt-20px">
+        <Checkbox
+          id="terms"
+          name="terms"
+          label={
+            <>
+              Megismertem és elfogadom az{' '}
+              <a
+                href="/adatvedelmi-tajekoztato/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="link-enhanced link-orange"
+              >
+                adatvédelmi tájékoztatót
+              </a>
+              , hozzájárulok nevem és email címem kezeléséhez.
+            </>
+          }
+          checked={isChecked}
+          onChange={(e) => setIsChecked(e.target.checked)}
+          className="orange"
+        />
+      </div>
+      <div className="row mt-20px">
+        <button type="submit" disabled={!isChecked} className="btn btn--full-width-mobile btn--secondary--solid">
+          Feliratkozás
+        </button>
+      </div>
+    </form>
+  );
+};
