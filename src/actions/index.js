32c2bda8fe598ee1c0331dea6c6903d4da095869
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-12-11 04:53:35 +0100

    test: netlify form test

--- src/actions/index.js
+++ src/actions/index.js
@@ -41,33 +41,3 @@ export const server = {
     },
   }),
 };
-
-// import { Resend } from 'resend';
-// import { defineAction } from 'astro:actions';
-// import { z } from 'astro:schema';
-
-// const resend = new Resend(import.meta.env.RESEND_API_KEY);
-
-// export const server = {
-//   contact: defineAction({
-//     input: z.object({
-//       name: z.string(),
-//       email: z.string().email(),
-//       comment: z.string(),
-//     }),
-//     accept: 'form',
-//     handler: async ({ name, email, comment }) => {
-//       try {
-//         const { data, error } = await resend.emails.send({
-//           from: 'Bandha Works Shala <shala@bandha.works>',
-//           to: email,
-//           subject: `Üzeneted megérkezett hozzánk`,
-//           html: `Hello, <br> <br> You have a new message`,
-//         });
-//         console.log(email, name, comment);
-//         console.log(data, error);
-//         return { ok: true };
-//       } catch (error) {}
-//     },
-//   }),
-// };
--- src/components/Forms/KapcsolatForm.tsx
+++ src/components/Forms/KapcsolatForm.tsx
@@ -41,7 +41,13 @@ export const KapcsolatForm = () => {
   };
 
   return (
-    <form name="contact bandhaworks 2025" onSubmit={handleSubmit} data-netlify="true" data-netlify-honeypot="bot-field">
+    <form
+      name="contact bandhaworks 2025"
+      onSubmit={handleSubmit}
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+      method="POST"
+    >
       <input type="hidden" name="form-name" value="contact bandhaworks 2025" />
       <div hidden>
         <input name="bot-field" />
