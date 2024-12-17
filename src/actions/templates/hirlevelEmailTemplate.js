53faa62ec24d8e983389c9ba43e63b78d56235cb
code-istvan <63305129+code-istvan@users.noreply.github.com>
2024-12-17 12:03:14 +0100

    feat: add English translation for the newsletter subscription email template

--- src/actions/templates/hirlevelEmailTemplate.js
+++ src/actions/templates/hirlevelEmailTemplate.js
@@ -1,12 +1,11 @@
 export const hirlevelEmailTemplate = {
   subject: 'Sikerült feliratkoznod a Bandha Works hírlevelére',
   content: (name, email) => `
-        <p>Kedves ${name},</p>
-    
-        <p>Köszönjük, hogy feliratkoztál hírlevelünkre</p>
-        <p>Az üzeneted tartalma:</p>
-        <blockquote>${email}</blockquote>
-        <p>Csapatunk az elkövetkező munkanapokon - általában 24-48 órán belül - feldolgozza az üzeneted, hamarosan válaszolunk.</p>
-        <p>Namasté,<br>Bandha Works Shala</p>
+        <p>Kedves ${name},</p><br>
+      <p>Köszönjük, hogy feliratkoztál a Bandha Works Jógaiskola hírlevelére!</p>
+      <p>A feliratkozás során megadott email-címed: <strong>${email}</strong></p>
+      <p>Mostantól első kézből értesülhetsz legfrissebb híreinkről, jógás eseményeinkről és különleges tartalmainkról.</p><br>
+      <p>Örülünk, hogy csatlakoztál közösségünkhöz!</p><br>
+      <p>Namasté,<br>Bandha Works Shala</p>
       `,
 };
--- src/actions/templates/newsletterEmailTemplate.js
+++ src/actions/templates/newsletterEmailTemplate.js
@@ -1,12 +1,11 @@
 export const newsletterEmailTemplate = {
   subject: 'You have successfully subscribed to the Bandha Works newsletter',
   content: (name, email) => `
-          <p>Dear ${name},</p>
-      
-         <p>Thank you for subscribing to our newsletter.</p>
-        <p>The content of your message:</p>
-        <blockquote>${email}</blockquote>
-        <p>Our team will process your message within the next working days – usually within 24-48 hours – and respond shortly.</p>
-        <p>Namasté,<br>Bandha Works Shala</p>
+         <p>Dear ${name},</p><br>
+            <p>Thank you for subscribing to the Bandha Works Yoga School newsletter!</p>
+            <p>The email address you provided during the subscription: <strong>${email}</strong></p>
+            <p>From now on, you will be the first to hear about our latest news, yoga events, and exclusive content.</p><br>
+            <p>We are happy to have you join our community!</p><br>
+            <p>Namaste,<br>Bandha Works Shala</p>
         `,
 };
