b44e35c487c7720b756016dad81978f4d28386e5
code-istvan <63305129+code-istvan@users.noreply.github.com>
2025-01-09 18:33:47 +0100

    feat: add automatic email sending to hirlevel and newsletter form

--- src/components/Forms/submitNewsletter.ts
+++ src/components/Forms/submitNewsletter.ts
@@ -1,58 +1,39 @@
-// import { actions } from 'astro:actions';
+import { actions } from 'astro:actions';
 
-// export const submitNewsletter = async (
-//   event: React.FormEvent<HTMLFormElement>,
-//   setLoading,
-//   setCheckboxClass,
-//   isChecked
-// ) => {
-//   event.preventDefault();
-//   setLoading(true);
+export const submitNewsletter = async (event: Event, language: 'hu' | 'en') => {
+  event.preventDefault();
+  console.log('submitNewsletter');
 
-//   if (!isChecked) {
-//     setCheckboxClass('red'); // Ha nincs bejelölve, váltson "red"-re
-//     setLoading(false);
-//     return;
-//   }
+  const formData = new FormData(event.target as HTMLFormElement);
+  const name = formData.get('name') as string | null;
+  const email = formData.get('email') as string | null;
 
-//   const formData = new FormData(event.target as HTMLFormElement);
-//   const name = formData.get('name') as string | null;
-//   const email = formData.get('email') as string | null;
-//   const language = formData.get('language') as string | null;
+  if (!name || !email || !language) {
+    console.error('Hiányzó kötelező mezők.');
+    return;
+  }
 
-//   if (!name || !email || !language) {
-//     console.error('Hiányzó kötelező mezők.');
-//     setLoading(false);
-//     return;
-//   }
+  const successPath = language === 'en' ? '/en/feliratkozas-sikeres/' : '/feliratkozas-sikeres/';
+  const errorPath = language === 'en' ? '/en/feliratkozas-sikertelen/' : '/feliratkozas-sikertelen/';
 
-//   // Meghatározza az átirányítási útvonalakat a `language` alapján
-//   const successPath = language === 'en' ? '/en/feliratkozas-sikeres/' : '/feliratkozas-sikeres/';
-//   const errorPath = language === 'en' ? '/en/feliratkozas-sikertelen/' : '/feliratkozas-sikertelen/';
+  try {
+    await fetch('/', {
+      method: 'POST',
+      body: formData,
+    });
+  } catch (error) {
+    console.error('Netlify form beküldés hiba:', error);
+  }
 
-//   // 1. Küldés Netlify felé
-//   try {
-//     await fetch('/', {
-//       method: 'POST',
-//       body: formData,
-//     });
-//   } catch (error) {
-//     console.error('Netlify form beküldés hiba:', error);
-//   }
-
-//   // 2. Email küldés
-//   try {
-//     const response = await actions.newsletter({ name, email, language });
-
-//     if (response.data?.data?.ok) {
-//       window.location.href = successPath;
-//     } else {
-//       window.location.href = errorPath;
-//     }
-//   } catch (error) {
-//     console.error('Hiba történt:', error);
-//     window.location.href = errorPath;
-//   } finally {
-//     setLoading(false);
-//   }
-// };
+  try {
+    const response = await actions.newsletter({ name, email, language });
+    if (response.data?.data?.ok) {
+      window.location.href = successPath;
+    } else {
+      window.location.href = errorPath;
+    }
+  } catch (error) {
+    console.error('Hiba történt:', error);
+    window.location.href = errorPath;
+  }
+};
