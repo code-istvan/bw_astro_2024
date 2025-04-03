fb3adf7ab8ef9b74df5f2c6aedd37950b8208f21
code-istvan <63305129+code-istvan@users.noreply.github.com>
2025-04-03 22:12:36 +0200

    feat: new English orarend page

--- src/actions/templates/mysoreEnEmailTemplate.js
+++ src/actions/templates/mysoreEnEmailTemplate.js
@@ -1,9 +1,13 @@
 export const mysoreEnEmailTemplate = {
   subject: 'Mysore Program Application',
   content: (fullName, comment, experienceLevel) => `
-        <h1>Mysore Program Application</h1>
-        <p><strong>Name:</strong> ${fullName}</p>
-        <p><strong>Experience Level:</strong> ${experienceLevel}</p>
-        <p><strong>Mysore experience:</strong> ${comment}</p>
+      <h1>Mysore Program Registration</h1>
+      <p>Dear ${fullName},</p>
+      <p>Thank you for your registration to our Mysore program!</p>
+      <p>We have received your application with the following content:</p>
+      <p><strong>Experience level:</strong> ${experienceLevel}</p>
+      <p><strong>Mysore experience:</strong> ${comment}</p>
+      <p>Our team will process your application during the upcoming workdays - usually within 24-48 hours - and we will respond soon.</p>
+      <p>Namaste,<br>Bandha Works Shala</p>
       `,
 };
