2b8e65fcfc6583b4ceefc4122434ff41c251e9d8
code-istvan <63305129+code-istvan@users.noreply.github.com>
2025-04-03 21:53:39 +0200

    feat: change mysore hu email template content

--- src/actions/templates/mysoreHuEmailTemplate.js
+++ src/actions/templates/mysoreHuEmailTemplate.js
@@ -2,8 +2,13 @@ export const mysoreHuEmailTemplate = {
   subject: 'Mysore-program jelentkezés',
   content: (fullName, comment, experienceLevel) => `
       <h1>Mysore Program Jelentkezés</h1>
-      <p><strong>Név:</strong> ${fullName}</p>
+       <p>Kedves ${fullName},</p>
+  
+      <p>Köszönjük, jelentkezésed Mysore-programunkba!</p>
+      <p>Jelentkezésed megkaptuk az alábbi tartalommal:</p>
       <p><strong>Tapasztalati szint:</strong> ${experienceLevel}</p>
       <p><strong>Mysore-tapasztalat:</strong> ${comment}</p>
+      <p>Csapatunk az elkövetkező munkanapokon - általában 24-48 órán belül - feldolgozza jelentkezésed, hamarosan válaszolunk.</p>
+      <p>Namasté,<br>Bandha Works Shala</p>
     `,
 };
